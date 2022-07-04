2e75e942b1a5ffd742e44709810fe7eb16d916f5
Satyam Gupta <satyamx811@gmail.com>
2022-07-04 16:24:29 +0530

    compelete work

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import './widgets/chart.dart';
+import './models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -11,33 +15,129 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'flutter app',
+      debugShowCheckedModeBanner: false,
+      title: 'personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        errorColor: Colors.red,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          headline6: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontFamily: 'OpenSans',
+            fontSize: 18
+          ),
+          button: TextStyle(
+            color: Colors.white
+          )
+        ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            headline6: TextStyle(
+               fontWeight: FontWeight.bold,
+               fontFamily: 'OpenSans',
+               fontSize: 20
+            )
+          )
+        ),
+      ),
       home: MyHomePage(),
     );
   }
 }
 
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({ Key? key }) : super(key: key);
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+//  String? titleInput;
+final List<Transaction> _userTransactions = [
+  //    Transaction(
+  //   id: 't1', 
+  //   title: 'new shoes', 
+  //   amount: 99.99, 
+  //   date: DateTime.now(),
+  // ),
+  // Transaction(
+  //   id: 't2', 
+  //   title: 'weekly grocery', 
+  //   amount: 20.09, 
+  //   date: DateTime.now(),
+  // ),
+  ];
+
+  List<Transaction> get _recentTransaction{
+    return _userTransactions.where((tx){
+      return tx.date!.isAfter(
+        DateTime.now().subtract(
+        Duration(days: 7)
+        ),
+        );
+    }).toList();
+  }
+
+  void _addNewTransaction(String txTitle, double txAmount, DateTime chosenDate ){
+    final newTx = Transaction(
+      title: txTitle, 
+      amount: txAmount,
+      date: chosenDate,
+      id: DateTime.now().toString()
+      );
+
+      setState(() {
+        _userTransactions.add(newTx);
+      });
+  }
+
+void _startAddNewTransaction(BuildContext ctx){
+  showModalBottomSheet(context: ctx, builder: (_){
+    return GestureDetector(
+      onTap: (){},
+      child: NewTransaction(_addNewTransaction),
+      behavior: HitTestBehavior.opaque,
+      );
+  },);
+}
+
+void _deleteTransaction(String id){
+  setState(() {
+    _userTransactions.removeWhere((tx) => tx.id == id);
+  });
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expenser track'),
-      ),
-      body: Column(
-        children: [
-          Card(
-            child: Text('First Card'),
-          ),
-
-          Card(
-            child: Text('Second card'),
-          )
+        title: Text('Expense track'),
+        actions: [
+          IconButton(
+            onPressed: () => _startAddNewTransaction(context), 
+            icon: Icon(Icons.add)
+            )
         ],
       ),
+      body: SingleChildScrollView(
+        child: Column(
+         // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Chart(_recentTransaction),
+            TransactionList(_userTransactions, _deleteTransaction),     
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _startAddNewTransaction(context),
+        child: Icon(Icons.add),
+        ),
    );
   }
 }
